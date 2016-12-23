e01a15172b54f2f2e917fb6ca4cd28c050822957
CarlosVernal <carlos.vernal.navarrete@gmail.com>
2016-12-23 11:56:24 -0300

    se crea una lista con los clientes premium

--- clientes/clientes.php
+++ clientes/clientes.php
@@ -5,6 +5,14 @@
     </head>
     <body>
     	<h1>Clientes</h1>
-    	<h3>Bienvenidos clientes</h3>
+    	<h3>Bienvenidos clientes Premium</h3>
+    	<p>Listado de clientes</p>
+    	<ol>
+    		<li>Carlos Vernal</li>
+    		<li>Pablo melendez</li>
+    		<li>Karina Paez</li>
+    		<li>Alexi Rodriguez</li>
+    		<li>Manuel Henriquez</li>
+    	</ol>
     </body>
 </html>
